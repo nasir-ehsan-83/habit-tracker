66aed6178250450d213e9a84503133df32daafcf
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-08-03 23:21:44 +0430

    refactor(model): remove 'refresh_token' from 'Users' model

--- habit-tracker/app/models/users.py
+++ habit-tracker/app/models/users.py
@@ -33,9 +33,6 @@ class User(Document):
     created_at:     datetime = Field(default_factory = lambda: datetime.now(timezone.utc))
     updated_at:     datetime = Field(default_factory = lambda: datetime.now(timezone.utc))
 
-    refresh_token:  str | None = None
-    
-
     class Settings:
     
         name = "users"
