f73639033247f39942e3abc9f08f43a20f7b4a3f
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-08-03 22:02:21 +0430

    feat(enum): add 'HabitCategory' enum

--- habit-tracker/app/utils/enum.py
+++ habit-tracker/app/utils/enum.py
@@ -1,17 +1,38 @@
 from enum import Enum
 
+
+
 class UserRole(str, Enum):
-    user    = "user"
-    admin   = "admin"
+
+    user        = "user"
+    admin       = "admin"
+
+
 
 class UserStatus(str, Enum):
-    active  = "active"
-    deleted = "deleted"
-    block   = "block"
+    
+    active      = "active"
+    deleted     = "deleted"
+    block       = "block"
+
+
 
 class HabitStatus(str, Enum):
+    
     pending     = "pending"
     completed   = "completed"
     skipped     = "skipped"
     deleted     = "deleted"
     archived    = "archived"
+
+
+
+class HabitCategory(str, Enum):
+
+    sport       = "sport"
+    study       = "study"
+    health      = "health"
+    finance     = "finance"
+    work        = "work"
+    routine     = "routine"
+    social      = "social"
