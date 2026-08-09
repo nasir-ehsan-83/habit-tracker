8f2bff81cf8bdd584475f084e44b73518636d3dc
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-08-09 21:54:22 +0430

    feat(admin): add '_route' suffix for all routes

--- habit-tracker/app/routes/admin.py
+++ habit-tracker/app/routes/admin.py
@@ -5,21 +5,28 @@ from typing import (
 from beanie import BeanieObjectId
 from fastapi import (
     APIRouter, 
-    Depends,
-    Path,
+    Depends, 
+    Path, 
     Query
 )
-from app.dependencies.current_user import get_current_user
-from app.dependencies.check_roles import require_role
-from app.models.habits import Habit
-from app.models.users import User
-from app.schemas.habits import HabitAdminOut
-from app.schemas.users import UserAdminOut
+
+from app.dependencies import (
+    get_current_user, 
+    required_role
+)
+from app.schemas import (
+    UserAdminOut, 
+    HabitAdminOut
+)
 from app.services.admin_service import (
-    block_user,
-    get_all_habits, 
-    get_all_users, 
-    get_one_user
+    block_user_service,
+    get_all_habits_service, 
+    get_all_users_service, 
+    get_user_service
+)
+from app.models import (
+    User,
+    Habit
 )
 from app.utils.enum import HabitCategory
 
@@ -31,7 +38,7 @@ router: APIRouter = APIRouter(
     tags = ["Admin"],
     dependencies = [
         Depends(get_current_user),
-        Depends(require_role(["ADMIN"]))
+        Depends(required_role(["ADMIN"]))
     ]
 )
 
@@ -42,12 +49,13 @@ router: APIRouter = APIRouter(
     '/users',
     response_model = List[UserAdminOut]
 )
-async def get_users(
-    is_active:  Annotated[bool, Query(default = False)], 
-    page:       Annotated[int, Query(default = 1,  gt = 0, lt = 100)],
+async def get_all_users_route(
+    is_active:  Annotated[bool, Query(default = True)], 
+    page:       Annotated[int, Query(default = 1, gt = 0, lt = 100)],
     limit:      Annotated[int, Query(default = 10, gt = 0, lt = 100)] 
-) -> List[User]:
-    return await get_all_users(is_active, page, limit)
+) -> List[User]: 
+
+    return await get_all_users_service(is_active, page, limit)
 
 
 
@@ -57,11 +65,11 @@ async def get_users(
     '/users/{user_id}',
     response_model = UserAdminOut
 )
-async def get_user(
+async def get_user_route(
     user_id:    Annotated[BeanieObjectId, Path()]
-) -> User | None:
-    
-    return await get_one_user(user_id)
+) -> User: 
+
+    return await get_user_service(user_id)
 
 
 
@@ -70,11 +78,11 @@ async def get_user(
     '/users/{user_id}/ban',
     response_model = UserAdminOut
 )
-async def user_ban(
+async def user_ban_route(
     user_id:    Annotated[BeanieObjectId, Path()]
-) -> User | None:
+) -> User: 
     
-    return await block_user(user_id)
+    return await block_user_service(user_id)
 
 
 
@@ -83,11 +91,11 @@ async def user_ban(
     '/habits', 
     response_model = List[HabitAdminOut]
 )
-async def get_habits(
+async def get_all_habits_route(
     owner_id:   Annotated[BeanieObjectId | None, Query(default = None)], 
-    category:   Annotated[HabitCategory, Query(default = None)],
+    category:   Annotated[HabitCategory | None, Query(default = None)], 
     page:       Annotated[int, Query(default = 1, gt = 0)], 
     limit:      Annotated[int, Query(default = 10, gt = 0)], 
-) -> List[Habit]:
-    
-    return await get_all_habits(owner_id, category, page, limit)
+) -> List[Habit]: 
+
+    return await get_all_habits_service(owner_id, category, page, limit)
