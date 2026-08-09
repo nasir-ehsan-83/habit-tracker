d9bddada547cd0f166d7e7d81c99feb499993965
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-08-09 20:35:41 +0430

    feat(model): add 'Streak' models/streaks.py

--- habit-tracker/app/models/__init__.py
+++ habit-tracker/app/models/__init__.py
@@ -1,10 +1,13 @@
 from .users import User
 from .habits import Habit
 from .tracks import Track
+from .preferences import UserPreference
+from .streaks import Streak
 
 
 __all__ = [
     "User",
     "Habit",
-    "Track"
+    "Track",
+    "UserPreference"
 ]
