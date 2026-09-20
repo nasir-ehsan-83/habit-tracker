f9829943fd099a821cadbc2cf785cf64eda0b339
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-09-20 12:04:42 +0430

    chore(ci): update workflow and update root endpoint test

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,13 +1,19 @@
-from fastapi.testclient import TestClient;
-from  app.main import app;
+import uuid
+from fastapi.testclient import TestClient
 
-client = TestClient(app);
+from app.main import app
+from app.schemas import UserCreate
 
-def test_root():
-    res = client.get("/");
 
-    print(res.json());
-    print(res.status_code);
-
-    assert res.json().get("message") == "hello world";
-    assert res.status_code == 200;
+def test_register() -> None:
+    with TestClient(app) as client:
+        uniq = uuid.uuid4().hex[:8]
+        user = UserCreate(
+            name = "Ahmad",
+            username = f"Ahmadi_{uniq}",
+            email = f"ahmad_{uniq}@example.com",
+            password = "password",
+        )
+        res = client.post("/api/auth/register", json = user.model_dump())
+        assert res.status_code == 201
+        assert res.json()["name"] == "Ahmad"
